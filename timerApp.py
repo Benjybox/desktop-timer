d12845eead0a43294c79dbd5a27d7ed706aa8b8c
Benjybox <b.gallagher@ucl.ac.uk>
2022-08-01 06:45:30 +0100

    Started by adding the start button, timer display box and a nice font etc. Next I need to start positioning the boxes, then get the timer function working

--- timerApp.py
+++ timerApp.py
@@ -1,6 +1,12 @@
 import kivy
 from kivy.app import App
 from kivy.app import Widget
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.boxlayout import BoxLayout
+
+def on_button_click(self):
+    print ("Timer Started")
+
 
 class MainWidget(Widget):
     pass
